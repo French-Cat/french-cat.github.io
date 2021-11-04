211cac740976de1dcaf310ed83b2e0581bbed6ef
Seth Carlson <89495053+DrNoSleep@users.noreply.github.com>
2021-11-04 09:50:01 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var box = document.getElementById('dvd'),
-  colors = ['#ff0000', '#ff4000', '#ff8000', '#ffbf00', '#ffff00', '#bfff00', '#80ff00', '#40ff00', '#00ff00', '#00ff40', '#00ff80', '#00ffbf', '#00ffff', '#00bfff', '#0080ff', '#0040ff', '#0000ff', '#4000ff', '#8000ff', '#bf00ff', '#ff00ff', '#ff00bf', '#ff0080', '#ff0040', '#ff0000'],
+  colors = ['#D12229', '#F68A1E', '#FDE01A', '#007940', '#24408E', '#732982', '#000000', '#078D70', '#26CEAA', '#98E8C1', '#FFFFFF', '#7BADE2', '#5049CC', '#3D1A78', '#0080ff', '#0040ff', '#0000ff', '#4000ff', '#8000ff', '#bf00ff', '#ff00ff', '#ff00bf', '#ff0080', '#ff0040', '#ff0000'],
   currentColor = Math.floor((Math.random() * 25) + 1),
   win = window,
   ww = win.innerWidth,
